a17a5be82b085d3e76032fa99d076fb72bc35fe4
1Rivali <fadyjonsina@gmail.com>
2024-07-17 01:16:23 +0300

    aa

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import * as proceess from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
+  app.enableCors({
+    origin: 'https://educate-iust-edu-l182j5s9n-1rivalis-projects.vercel.app', // or '*' to allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable this if you need to pass cookies with requests
+  });
   await app.listen(process.env.PORT!, '0.0.0.0');
 }
 bootstrap();
