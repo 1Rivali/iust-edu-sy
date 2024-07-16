f1809355dd5ee36590a2c7fa9fbc69ab53a95393
1Rivali <fadyjonsina@gmail.com>
2024-07-16 23:49:17 +0300

    login logger

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -22,6 +22,7 @@ export class StudentService {
 
   async login(loginDto: LoginDto) {
     const student = await this.findOne(loginDto.username);
+    console.log(student);
     if (student?.password !== loginDto.password) {
       throw new UnauthorizedException();
     }
