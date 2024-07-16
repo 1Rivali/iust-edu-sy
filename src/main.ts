73a4c4288969a987a0143d9ba4ba89ecf819fc74
1Rivali <fadyjonsina@gmail.com>
2024-07-17 01:12:46 +0300

    aa

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as proceess from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
   await app.listen(process.env.PORT!, '0.0.0.0');
 }
 bootstrap();
