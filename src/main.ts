a8650b436142e8d5ba0160542714906a0a2e3259
1Rivali <fadyjonsina@gmail.com>
2024-07-17 00:33:58 +0300

    database

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as proceess from 'process';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
-  await app.listen(process.env.PORT!, '0.0.0.0');
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
